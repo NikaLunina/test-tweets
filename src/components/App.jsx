dfcca948cb0645b5e63f64b066a11b8cc5ba341e
Nika <99656127+NikaLunina@users.noreply.github.com>
2023-05-24 22:39:05 +0300

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+i
+
+const Home = lazy(() => import('../pages/Home'));
+const Tweets = lazy(() => import('../pages/Tweets'));
+const NotFound = lazy(() => import('../components/NotFound/NotFound'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+        <Header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/movies">Tweets</Link>
+              </li>
+            </ul>
+          </nav>
+        </Header>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+        <Route path="/tweets" element={<Tweets />} />
+        <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </Container>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div>
+      <p>
+       Welcome to our application!
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
