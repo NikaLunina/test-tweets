2b81ee3630e3c453c82dbe3a3478cda86dff5fee
Nika <99656127+NikaLunina@users.noreply.github.com>
2023-05-25 13:01:56 +0300

    back to start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
-
-
-const Home = lazy(() => import('../pages/Home'))
-const Tweets = lazy(() => import('../pages/Tweets'));
-const NotFound = lazy(() => import('../components/NotFound'));
-
 export const App = () => {
   return (
-    <container>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/movies">Tweets</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-        <Route path="/tweets" element={<Tweets />} />
-        <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
-      </container>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
