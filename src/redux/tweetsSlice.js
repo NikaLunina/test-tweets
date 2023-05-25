be1ff4f637b4cf5da3a91b3a45fe0510ddd2a445
Nika <99656127+NikaLunina@users.noreply.github.com>
2023-05-25 22:03:07 +0300

    update index.js

--- src/redux/tweetsSlice.js
+++ src/redux/tweetsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { fetchGetCard  , fetchUpdateCard} from './Options';
+import { fetchGetCard  , fetchUpdateCard} from './operations';
 
 const TweetsSlice = createSlice({
   name: 'card',
